6801c13ef8fd6a248b8955d83e4b7e8de98d01fe
GCan5 <168529364+GCan5@users.noreply.github.com>
2024-05-08 14:44:15 +0200

    more Calculations
    - Totaltemperatur 1 vor Verdichter
    - Problems solved with adressing the Multiindex

--- convert.py
+++ convert.py
@@ -12,13 +12,26 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
+# Definition der Konstanten außerhalb der Funktion main()
+K_GC_R_LUFT = 287.2
+KEN_AREA_TC1 = 1963.495408
+K_GC_P_AIR = 1005.2
+K_GC_KAPPA_AIR = 1.4
+
+
+
 
 @click.command()
 @click.option("--path", prompt="The path", help="Location of files")
-@click.option("--prefix", prompt="The file prefix pattern", help="File prefix pattern")
+@click.option("--prefix", prompt="The file prefix pattern",default= "1khz", help="File prefix pattern")
 @click.option("--skip-header", default=2, help="Number header lines to skip")
 @click.option("--write-excel", default=True, help="Write results to excel files")
 @click.option("--write-pdf", default=False, help="Plot results to pdf file")
+
+
+
+
+
 def main(path, prefix, skip_header, write_excel, write_pdf):
     results_mean = []
     results_std = []
@@ -46,24 +59,46 @@ def main(path, prefix, skip_header, write_excel, write_pdf):
             date_format="%d-%m-%Y %H:%M:%S.%f",
             na_values={"?"},
         )
-        # Calculate mean and store result
-        # Berechne die neuen Kanäle
+        
         # Berechne die neuen Kanäle und füge sie direkt zum DataFrame hinzu
+        print(df.columns)
         df["Q_P_LE/P1"] = df["P_TC_IN_LE"] / df["P_TC_1"]
-        df["T_LE-T_1_2"] = df["T_TC_IN_LE"] / df["T_TC_1_2"]
-        df["T2-T_LE"] = df["T_TC_2"] / df["T_TC_IN_LE"]
-        df["T2-T_1_2"] = df["T_TC_2"] / df["T_TC_1_2"]
+        df["T_LE-T_1_2"] = df["T_TC_IN_LE"] - df["T_TC_1_2"]
+        df["T2-T_LE"] = df["T_TC_2"] - df["T_TC_IN_LE"]
+        df["T2-T_1_2"] = df["T_TC_2"] - df["T_TC_1_2"]
+        # Print out individual components of the calculation
+        print("P_amb",df["P_Amb"]["mbar"])
+        
+        print(("Konstante"),K_GC_R_LUFT)
+        # Calculate intermediate results
 
+        intermediate_result = df["P_TC_1"]["bar"] * 100000
+        print("intermediate_result", intermediate_result)
+        intermediate_result_1 = intermediate_result + df["P_Amb"]["mbar"]
+        print("intermediate_result_1", intermediate_result_1)
+        intermediate_result_2= intermediate_result_1 / (K_GC_R_LUFT * (df["T_Amb"]["°C"] + 273.15))
+        print(intermediate_result_2)
+
+        # # Assign the intermediate result to the new column
+        df["Rho_TC_1_C"] = intermediate_result_2
+        #df.loc[0:,"Rho_TC_1_C"] = ( df.loc[0:,"P_TC_1"] * 100000 + df.loc[0:,"P_Amb"]) / (K_GC_R_LUFT * ( df.loc[0:,"T_Amb"] + 273.15))
+        #df["Rho_TC_1_C"] = (df["P_TC_1"] * 100000 + df["P_Amb"]) / (K_GC_R_LUFT * (df["T_Amb"] + 273.15))
+     
+        df["Vel_TC_1_C"] = df["m_HFM"] ["kg/h"]/ 3600 / df["Rho_TC_1_C"] / (KEN_AREA_TC1 / 10**6)
+        df["Totaltemperatur_1"] = df["T_Amb"]["°C"] + 273.15 + (df["Vel_TC_1_C"]**2) / (2 * K_GC_P_AIR)
+        print(("RESULT"),df.Totaltemperatur_1)
+        
         mean_values = df.mean(numeric_only=True)
-        print(mean_values)
+
+        #print(mean_values)  
         results_mean.append(mean_values)
         # Calc std() and store result
         results_std.append(df.std(numeric_only=True))
+        
 
     # Merge mean() results
     mdf = pd.concat(results_mean, axis=1)
-    # Subtract values from "P_TC_2" channel from "P_TC_1" channel and store the result in a new column
-    # mdf["P_TC_2_minus_P_TC_1"] = mdf["P_TC_1"] - mdf["P_TC_2"]
+
     if write_excel:
         mdf.to_excel(os.path.join(output_dir, "mean.xlsx"))
         logger.info(f"Created mean.xlsx in {output_dir}")
